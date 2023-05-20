99ade684bc39083731749aba7ffb8eb57153242a
Volodymyr Marchuk <vvmarchuk1984@gmail.com>
2023-05-20 21:43:53 +0300

    Use cash and catcha

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -25,6 +25,7 @@ from women.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', include('women.urls')),
 ]
 if settings.DEBUG:
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
 from .forms import *
@@ -61,9 +61,23 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 #    return render(request, 'women/addpage.html', {'form': form, 'title': 'Додай статтю'})
 
 
-def contact(request):
-    return render(request, 'women/contact.html', {'title': 'Зворотній звязок', 'menu': menu})
+# def contact(request):
+#     return render(request, 'women/contact.html', {'title': 'Зворотній звязок', 'menu': menu})
 
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'women/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, object_list=None ,**kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Зворотній зв'язок")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
 
 #def login(request):
 #    return render(request, 'women/login.html', {'title': 'Увійти', 'menu': menu})
